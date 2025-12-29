b8d20a8ec6936c69400b728d23f76a28095e862d
Nazar Kyselov <kyselovno@gmail.com>
2025-12-29 20:12:23 +0100

    feat: marker
    only HIT version

---
+++ components/ui/marker.tsx
@@ -0,0 +1,16 @@
+﻿import { Fire20SolidIcon } from "../icons/heroicons-fire-20-solid";
+
+export interface MarkerProps {
+   name: 'hit' | undefined;
+   size: 'md' | 'sm' | undefined;
+}
+
+export default function Marker({name, size}: MarkerProps) {
+   return (
+      <div className="size-6 flex justify-center items-center rounded-[8px] bg-red-600">
+         {name == 'hit' && (
+            <Fire20SolidIcon size={16} color="#ffffff"></Fire20SolidIcon>
+         )}
+      </div>
+   );
+}
