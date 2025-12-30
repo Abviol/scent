1854d8c08b7dd322d26e5429820eb9831da333d8
Nazar Kyselov <kyselovno@gmail.com>
2025-12-30 19:28:04 +0100

    feat: utils.ts
    - volEnumToNumber utility function

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,26 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+import { VolumeEnum, VolumeType } from "./types";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+	return twMerge(clsx(inputs));
+}
+
+/* 
+  @param vol volume enum value
+  Converts volume enum to its numeric value. 
+*/
+export function volEnumToNumber(vol: VolumeEnum): VolumeType {
+	switch (vol) {
+		case 0:
+			return 30;
+		case 1:
+			return 50;
+		case 2:
+			return 100;
+		case 3:
+			return 200;
+    default:
+      return undefined;
+	}
 }
