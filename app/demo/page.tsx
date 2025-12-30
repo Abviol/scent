ccc05b48513523e5c354ecb6b8991297d9df18c5
Nazar Kyselov <kyselovno@gmail.com>
2025-12-30 19:35:16 +0100

    feat: /demo page
    Will contain all components for pre-view

---
+++ app/demo/page.tsx
@@ -0,0 +1,16 @@
+﻿import ProductCard from "@/components/productCard";
+import Marker from "@/components/ui/marker";
+
+export default function Page() {
+   return (
+      <div className="container py-12 px-8">
+         <div className="flex">
+            <Marker name="hit" size="sm"></Marker>
+         </div>
+         <div className="flex gap-x-4">
+            <ProductCard title={"Versace Eros Flame"} image_url={"https://i.makeup.it/u/uf/uf0jgxb7gg2e.jpg"} image_alt="asdsadf" markers="hit" wishlist={false} volume={0} rating={4.5} price={200}/>
+         </div>
+      </div>
+
+   )
+}
