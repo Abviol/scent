83fad53e91cec806197ab7106a4b754cef5acfe9
Nazar Kyselov <kyselovno@gmail.com>
2025-12-30 19:28:54 +0100

    feat: Rating component (demo)
    create layout

---
+++ components/ui/rating.tsx
@@ -0,0 +1,16 @@
+﻿import { Star } from "lucide-react";
+
+export interface RatingProps {
+   rating: number;
+}
+
+export default function Rating({ rating }: RatingProps ){
+   return(
+      <div className="flex gap-x-1.5"> 
+         <span className="my-text-p">{rating}</span>
+         <div className="h-full flex items-center">
+            <Star size={12} strokeWidth={3} />
+         </div>
+      </div>
+   )
+}
