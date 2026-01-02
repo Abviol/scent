a84babea8039603cbd078e777e905a31e96a5cf4
Nazar Kyselov <kyselovno@gmail.com>
2026-01-02 17:49:21 +0100

    feat(components): SearchCard
    add to demo

--- app/demo/page.tsx
+++ app/demo/page.tsx
@@ -1,9 +1,10 @@
 ﻿import ProductCard from "@/components/productCard";
+import SearchCard from "@/components/searchCard";
 import Marker from "@/components/ui/marker";
 
 export default function Page() {
 	return (
-		<div className="container py-12 px-8">
+		<div className="py-12 px-8">
 			<div className="flex mb-10">
 				<Marker name="hit" size="sm"></Marker>
 			</div>
@@ -21,7 +22,7 @@ export default function Page() {
 						price={200}
 					/>
 
-               <ProductCard
+					<ProductCard
 						title={"Versace Eros Flame"}
 						image_url={"https://i.makeup.it/u/ux/uxuxdj4ehyen.jpg"}
 						markers={["hit", "hit"]}
@@ -31,10 +32,9 @@ export default function Page() {
 						price={9999}
 					/>
 
-                <ProductCard
+					<ProductCard
 						title={"Versace Eros Flame"}
 						image_url={"https://i.makeup.it/2/2p/2pj8d0xfdqe0.jpg"}
-						image_alt="asdsadf"
 						markers={[]}
 						wishlist={true}
 						volume={0}
@@ -42,6 +42,28 @@ export default function Page() {
 						price={200}
 					/>
 				</div>
+			</div>
+
+			<h3 className="my-text-lg">Search card</h3>
+			<div className="flex gap-x-4">
+				<SearchCard
+					title={"Versace Eros Flame"}
+					productId="01203213"
+					quantityInStock={10}
+					image_url={"https://i.makeup.it/2/2p/2pj8d0xfdqe0.jpg"}
+					volume={0}
+					rating={4.5}
+					price={4999}
+				></SearchCard>
+				<SearchCard
+					title={"Versace Eros Flame"}
+					productId="01203213"
+					quantityInStock={0}
+					image_url={"https://i.makeup.it/2/2p/2pj8d0xfdqe0.jpg"}
+					volume={0}
+					rating={4.5}
+					price={4999}
+				></SearchCard>
 			</div>
 		</div>
 	);
---
+++ components/searchCard.tsx
@@ -0,0 +1,74 @@
+﻿"use client";
+
+import { AvailabilityType, VolumeEnum } from "@/lib/types";
+import { getAvailability, getEuro, volEnumToNumber } from "@/lib/utils";
+import Image from "next/image";
+import Link from "next/link";
+import Rating from "./ui/rating";
+import { useState } from "react";
+
+export interface SearchCardProps {
+	title: string;
+	productId: string;
+	volume: VolumeEnum;
+	rating: number;
+	price: number; //cents
+	quantityInStock: number;
+	image_url: string;
+}
+
+export default function SearchCard({
+	title,
+	productId,
+	volume,
+	rating,
+	price,
+	quantityInStock,
+	image_url,
+}: SearchCardProps) {
+   const [availability, setAvailability] = useState<AvailabilityType>(getAvailability(quantityInStock));
+   let availabilityClass: string;
+   switch (availability) {
+      case "available":
+         availabilityClass = "text-accent";
+         break;
+      default:
+         availabilityClass = "text-gray-500";
+         break;
+   }
+
+	return (
+		<Link href={"/"} className="w-full max-w-[500px] flex gap-x-10 p-5 rounded-[8px] bg-white transition-colors hover:bg-accent-light">
+			<div className="relative overflow-hidden size-[100px] aspect-square flex shrink-0 justify-center items-center">
+				<Image
+					src={image_url}
+					alt={`${title} ${volEnumToNumber(volume)} ml`}
+					fill
+					className="object-contain"
+					loading="eager"
+					priority={false}
+				></Image>
+			</div>
+
+         <div className="w-full flex flex-col justify-between">
+            <div className="flex flex-row w-full justify-between">
+               <h3 className="font-semibold whitespace-nowrap">{title}</h3>
+               <span className="font-semibold">{productId}</span>
+            </div>
+
+            <div className="flex justify-between">
+               <span className="font-[14px] gap-x-10 items-center">
+						{volEnumToNumber(volume)} ml
+					</span>
+               <Rating rating={rating}></Rating>
+            </div>
+
+            <div className="flex justify-between">
+               <span className="font-semibold">{getEuro(price)}</span>
+               <span className={`font-semibold capitalize ${availabilityClass}`}>{availability}</span>
+
+            </div>
+         </div>
+		</Link>
+	);
+}
