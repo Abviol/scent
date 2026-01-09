bc4f78bc0c192a366bd1044e43e9d02c52fa7b20
Nazar Kyselov <kyselovno@gmail.com>
2026-01-09 18:57:05 +0100

    chore(next.config.ts): add github.com
    github.co

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [new URL('https://i.makeup.it/**'), new URL('https://lh3.googleusercontent.com/**')],
-  },
+	images: {
+		remotePatterns: [
+			new URL("https://i.makeup.it/**"),
+			new URL("https://lh3.googleusercontent.com/**"),
+      new URL("https://github.com/**"),
+		],
+	},
 };
 
 export default nextConfig;
