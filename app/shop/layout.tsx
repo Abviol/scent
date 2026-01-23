aa9a567c35f0b4683b1f37b6c361e9b104ce89e1
Nazar Kyselov <kyselovno@gmail.com>
2026-01-23 13:07:48 +0100

    feat(shop): layout
    header and footer

---
+++ app/shop/layout.tsx
@@ -0,0 +1,12 @@
+﻿import Footer from "@/components/layout/footer";
+import Header from "@/components/layout/header";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+	return (
+		<>
+			<Header />
+			<div className="container mx-auto px-8">{children}</div>
+			<Footer />
+		</>
+	);
+}
