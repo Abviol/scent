8969e4a7067ab28edb2c3b22081670186d69e382
Nazar Kyselov <kyselovno@gmail.com>
2026-02-02 22:35:20 +0100

    feat(productPage): Product Gallery

---
+++ app/shop/product/[id]/productGallery.tsx
@@ -0,0 +1,137 @@
+﻿"use client";
+
+import Image from "next/image";
+import { ReactNode, useState } from "react";
+import { ChevronDown, ChevronUp } from "lucide-react";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import { type Swiper as SwiperType } from "swiper";
+import { Mousewheel } from "swiper/modules";
+
+import "swiper/css";
+
+interface ProductGalleryProps {
+	imageUrls: string[];
+	title: string;
+}
+
+export default function ProductGallery({
+	imageUrls,
+	title,
+}: ProductGalleryProps) {
+	const [activeImageIdx, setActiveImageIdx] = useState(0);
+	const [swiperInstance, setSwiperInstance] = useState<SwiperType | null>(
+		null,
+	);
+
+	const [slideStatus, setSlideStatus] = useState({
+		isBeginning: true,
+		isEnd: false,
+	});
+
+	if (!imageUrls?.length) return null;
+	
+	const handleSlideChange = (swiper: SwiperType) => {
+		setSlideStatus({
+			isBeginning: swiper.isBeginning,
+			isEnd: swiper.isEnd,
+		});
+	};
+
+	return (
+		<div className="grid grid-cols-[72px_1fr] gap-2 h-[500px]">
+			<div className="min-h-0 flex flex-col h-full w-full gap-2">
+				{/* Up Button */}
+				<NavButton
+					onClick={() => swiperInstance?.slidePrev()}
+					ariaLabel="Previous thumbnail"
+					disabled={slideStatus.isBeginning}
+				>
+					<ChevronUp size={16} color="currentColor" />
+				</NavButton>
+
+				<div className="flex-1 min-h-0 overflow-hidden">
+					<Swiper
+						direction="vertical"
+						spaceBetween={8}
+						slidesPerView={"auto"}
+						onSwiper={(swiper) => {
+							setSwiperInstance(swiper);
+							handleSlideChange(swiper);
+						}}
+						onSlideChange={handleSlideChange} 
+						modules={[Mousewheel]}
+						mousewheel={true}
+						className="h-full w-full"
+					>
+						{imageUrls.map((img, index) => (
+							<SwiperSlide
+								key={`${img} ${index}`}
+								className="h-auto! w-full!"
+							>
+								<button
+									onClick={() => setActiveImageIdx(index)}
+									className={`relative aspect-square w-full overflow-hidden rounded-md border transition-all ${
+										activeImageIdx === index
+											? "border-accent border-2"
+											: "opacity-70 hover:opacity-100 border-transparent"
+									}`}
+								>
+									<Image
+										src={img}
+										alt={`${title} thumbnail ${index}`}
+										fill
+										className="object-contain"
+									/>
+								</button>
+							</SwiperSlide>
+						))}
+					</Swiper>
+				</div>
+
+				{/* Down Button */}
+				<NavButton
+					onClick={() => swiperInstance?.slideNext()}	 
+					ariaLabel="Next thumbnail"
+					disabled={slideStatus.isEnd}
+				>
+					<ChevronDown size={16} color="currentColor" />
+				</NavButton>
+			</div>
+
+			{/* Active image */}
+			<div className="relative aspect-square w-full overflow-hidden rounded-lg">
+				<Image
+					src={imageUrls[activeImageIdx]}
+					alt={title}
+					fill
+					className="object-contain"
+					priority
+				/>
+			</div>
+		</div>
+	);
+}
+
+function NavButton({
+	onClick,
+	children,
+	ariaLabel,
+	disabled,
+}: {
+	onClick: () => void;
+	children: ReactNode;
+	ariaLabel: string;
+	disabled: boolean;
+}) {
+	return (
+		<button
+			onClick={onClick}
+			className="flex h-6 w-full items-center justify-center text-main rounded-md hover:bg-gray-100 disabled:opacity-50 disabled:cursor-not-allowed transition-colors"
+			aria-label={ariaLabel}
+			disabled={disabled}
+		>
+			{children}
+		</button>
+	);
+}
