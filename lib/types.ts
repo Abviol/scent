67055a3180e77200e7a52d34dd5a6993b96d7897
Nazar Kyselov <kyselovno@gmail.com>
2026-02-07 11:48:48 +0100

    feat(types): add ProductType

--- lib/types.ts
+++ lib/types.ts
@@ -1,8 +1,32 @@
 ﻿export enum VolumeEnum {
-  ML30,
-  ML50,
-  ML100,
-  ML200,
+	ML30,
+	ML50,
+	ML100,
+	ML200,
+}
+
+export interface ProductType {
+	id: string;
+	code: number;
+	title: string;
+	rating: number;
+	reviewsAmount: number;
+	type: string;
+	tags: string[];
+	categories: string[];
+	brand: string;
+	imageUrls: string[];
+	variants: number[];
+	selectedVariant: number;
+	wishlist: boolean;
+	price: number;
+	discounted: boolean;
+	discountedPrice: number;
+	description: string;
+	details: Record<string, string | number>;
+	ingredients: string;
+	quantityInStock: number;
+  markers: MarkerType[]
 }
 
 export type VolumeType = 30 | 50 | 100 | 200 | undefined;
