ea48385058513b4b9c3e4fd66f0bf61452cbf421
Nazar Kyselov <kyselovno@gmail.com>
2026-02-09 17:04:22 +0100

    chore(tag): refactor
    - add "id" prop
    - rename "text" prop to "label"
    - rename "onRemove" to "onClick"
    - make the whole component clickable

--- components/ui/tag.tsx
+++ components/ui/tag.tsx
@@ -1,18 +1,27 @@
-﻿import React from 'react';
-import { X } from 'lucide-react';
+﻿import React from "react";
+import { X } from "lucide-react";
 
 export interface TagProps {
-   text: string;
-   onRemove: () => void;
+	label: string;
+  id: string;
+	onClick: (id: string) => void;
 }
 
-export default function Tag ({ text, onRemove }: TagProps) {
-  return (
-    <div className="flex items-center justify-between gap-5 rounded-md bg-accent-light px-3.5 py-2.5">
-      <span className="font-semibold text-main">{text}</span>
-      <button onClick={onRemove} className="flex items-center justify-center text-main hover:text-opacity-75">
-        <X size={16} strokeWidth={3} />
-      </button>
-    </div>
-  );
-};
+export default function Tag({ label, id, onClick }: TagProps) {
+  const handleClick = () => {
+    onClick(id);
+  }
+
+	return (
+		<div
+			role="button"
+			onClick={handleClick}
+			className="h-9 flex items-center justify-between gap-5 rounded-md bg-accent-light px-3.5 py-2.5 cursor-pointer"
+		>
+			<span className="font-semibold text-main text-base">{label}</span>
+			<div className="flex items-center justify-center text-main hover:text-opacity-75">
+				<X size={16} strokeWidth={3} />
+			</div>
+		</div>
+	);
+}
