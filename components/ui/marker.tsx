2a6ab879ca6d16afc89936559808533cd97c375f
Nazar Kyselov <kyselovno@gmail.com>
2026-03-23 20:39:30 +0100

    fix(marker): always truthy expression

--- components/ui/marker.tsx
+++ components/ui/marker.tsx
@@ -9,7 +9,7 @@ export interface MarkerProps {
 export default function Marker({name, size}: MarkerProps) {
    return (
       <div className={cn("flex justify-center items-center rounded-[8px] bg-red-600",
-            size === 'sm' ? "size-5" : 'md' ? "size-6" : "size-6",
+            size === 'sm' ? "size-5" : size === 'md' ? "size-6" : "size-6",
           )}>
          {name == 'hit' && (
             <FireIcon size={16} color="#ffffff"></FireIcon>
