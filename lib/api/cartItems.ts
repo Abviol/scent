793f010b6c9ad8fa0a9b4b14aff3e78293cf94be
Nazar Kyselov <kyselovno@gmail.com>
2026-04-13 16:28:34 +0200

    Update cartItems.ts

--- lib/api/cartItems.ts
+++ lib/api/cartItems.ts
@@ -3,7 +3,7 @@ import {CART_ITEMS} from "../data";
 export default async function getCartItems(userId: string) {
     // Simulatel DB network delay
     await new Promise((resolve) => setTimeout(resolve, 500));
-    console.log("Cart items fetched successfully.");
+    console.log(`Cart items of the user ${userId} fetched successfully.`);
 
     return CART_ITEMS;
 }
