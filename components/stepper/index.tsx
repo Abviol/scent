a9fc52cba65bac4052ca67d08f1bbd51c10dfd1a
Nazar Kyselov <kyselovno@gmail.com>
2026-07-08 23:09:25 +0200

    refactor(stepper): onChange's prop name
    Rename the prop of the onChange prop from `newValue` to `delta`
    as soon as the logic that's supposed to use the component
    needs to need the difference of changed value (the delta).

--- components/stepper/index.tsx
+++ components/stepper/index.tsx
@@ -1,72 +1,67 @@
 ﻿/* icons */
-import { Minus, Plus } from "lucide-react";
+import {Minus, Plus} from "lucide-react";
 /* styles */
 import "./styles.css";
 
 interface StepperProps {
-	value: number;
-	min?: number;
-	max?: number;
-	step?: number;
-	disabled?: boolean;
-	size?: "md" | "sm";
-	onChange: (newValue: number) => void;
+    value: number;
+    min?: number;
+    max?: number;
+    step?: number;
+    disabled?: boolean;
+    size?: "md" | "sm";
+    onChange: (delta: number) => void;
 }
 
+
 export default function Stepper({
-	value,
-	min = 1,
-	max = 99,
-	step = 1,
-	disabled = false,
-	size = "md",
-	onChange,
-}: StepperProps) {
-	const handleDecrement = (): void => {
-		onChange(Math.max(min, value - step));
-	};
-	const handleIncrement = (): void => {
-		onChange(Math.min(max, value + step));
-	};
+                                    value,
+                                    min = 1,
+                                    max = 99,
+                                    step = 1,
+                                    disabled = false,
+                                    size = "md",
+    								onChange,
+                                }: StepperProps) {
 
-	const sizeClass = {
-		md: "stepper_md",
-		sm: "stepper_sm"
-	};
+    const sizeClass = {
+        md: "stepper_md",
+        sm: "stepper_sm"
+    };
 
-	return (
-		<div className={`${sizeClass[size]} flex relative`}>
-			<button
-				aria-label="Decrement"
-            title="Decrement"
-				onClick={handleDecrement}
-            disabled={disabled || value <= min}
-				className="group border-2 border-gray-200 flex justify-center items-center disabled:cursor-not-allowed"
-            type="button"
-			>
-				<Minus
-					size={size == "md" ? 32 : 18}
-					strokeWidth={1.5}
-					className="stepper__icon group-hover:stroke-accent group-disabled:stroke-gray-200 transition-colors"
-				></Minus>
-			</button>
-			<div className="stepper__display h-full flex justify-center items-center">
-				<span className={` ${disabled && 'text-gray-200'}`}>{value}</span>
-			</div>
-			<button
-				aria-label="Increment"
-            title="Increment"
-				onClick={handleIncrement}
-            disabled={disabled || value >= max}
-				className="group border-2 border-gray-200 flex justify-center items-center disabled:cursor-not-allowed"
-            type="button"
-			>
-				<Plus
-					size={size == "md" ? 32 : 18}
-					strokeWidth={1.5}
-					className="group-hover:stroke-accent group-disabled:stroke-gray-200 transition-colors"
-				></Plus>
-			</button>
-		</div>
-	);
+    return (
+        <div className={`${sizeClass[size]} flex relative`}>
+            <button
+                aria-label="Decrement"
+                title="Decrement"
+                onClick={() => onChange((-1) * step)}
+                disabled={disabled || value <= min}
+                className="group border-2 border-gray-200 flex justify-center items-center disabled:cursor-not-allowed"
+                type="button"
+            >
+                <Minus
+                    size={size == "md" ? 32 : 18}
+                    strokeWidth={1.5}
+                    className="stepper__icon group-hover:stroke-accent group-disabled:stroke-gray-200 transition-colors"
+                ></Minus>
+            </button>
+            <div className="stepper__display h-full flex justify-center items-center">
+                <span className={` ${disabled && 'text-gray-200'}`}>{value}</span>
+            </div>
+            <button
+                aria-label="Increment"
+                title="Increment"
+                onClick={() => onChange(step)}
+                disabled={disabled || value >= max}
+                className="group border-2 border-gray-200 flex justify-center items-center disabled:cursor-not-allowed"
+                type="button"
+            >
+                <Plus
+                    size={size == "md" ? 32 : 18}
+                    strokeWidth={1.5}
+                    className="group-hover:stroke-accent group-disabled:stroke-gray-200 transition-colors"
+                ></Plus>
+            </button>
+        </div>
+    );
 }
